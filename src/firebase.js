98b76ec1226b4fd8bddd3bd7c71e07f9540be33f
sakshi-kurariya <sakshikurariya2017@gmail.com>
2024-11-19 15:32:26 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDghuaGakiI9A2CDI204f5XZrba48ee1x4",
+    authDomain: "dream-home-a36e2.firebaseapp.com",
+    projectId: "dream-home-a36e2",
+    storageBucket: "dream-home-a36e2.appspot.com",
+    messagingSenderId: "966588809666",
+    appId: "1:966588809666:web:b66152a59f292c4faf565b"
+  };
